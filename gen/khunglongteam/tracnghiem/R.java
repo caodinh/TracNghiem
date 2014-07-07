626b03a1b973fe62dfa0eae04dc6e3977ecef6f9
caodinh <dinhhv93@gmail.com>
2014-07-08 00:15:25 +0700

    Đã tạo xong 5 file XML câu hỏi

--- gen/khunglongteam/tracnghiem/R.java
+++ gen/khunglongteam/tracnghiem/R.java
@@ -22,12 +22,20 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_cau1=0x7f030000;
-        public static final int activity_main=0x7f030001;
+        public static final int activity_cau2=0x7f030001;
+        public static final int activity_cau3=0x7f030002;
+        public static final int activity_cau4=0x7f030003;
+        public static final int activity_cau5=0x7f030004;
+        public static final int activity_main=0x7f030005;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int title_activity_cau1=0x7f040002;
+        public static final int title_activity_cau2=0x7f040003;
+        public static final int title_activity_cau3=0x7f040004;
+        public static final int title_activity_cau4=0x7f040005;
+        public static final int title_activity_cau5=0x7f040006;
     }
     public static final class style {
         /** 
---
+++ src/khunglongteam/tracnghiem/Cau4Activity.java
@@ -0,0 +1,15 @@
+package khunglongteam.tracnghiem;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Cau4Activity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_cau4);
+	}
+}
